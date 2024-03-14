69aba2a380f8d7eaa5ec6b0b0ba2bd81110ab14e
Charlie Brewis <charliebrewis7@gmail.com>
2024-03-14 16:56:00 +0000

    B1 part 1 started

--- 1 - Speed/Part B/B1/js/script.js
+++ 1 - Speed/Part B/B1/js/script.js
@@ -1,1 +1,12 @@
 //Put your code here
+
+const canvas = document.getElementById('img-canvas');
+const ctx = canvas.getContext('2d');
+
+const img = new Image();
+img.src = "img/athena.jpg";
+
+img.onload = function() {
+    //TODO: figure out better resizing
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+};
